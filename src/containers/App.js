3f06134c568d0fb79dafaaa086c92768cb99def4
Luis Olivar <lao294@nyu.edu>
2018-10-28 00:44:18 -0400

    Added Firebase

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Router from "react-router-dom/BrowserRouter"
 import Pages from "../pages"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import BottomNav from "../components/BottomNav"
+import firebase from 'firebase'
 
 const styles = {
   app: { minHeight: "100%"},
@@ -12,6 +13,32 @@ const styles = {
 }
 
 class App extends Component {
+
+  componentDidMount() {
+    const config = {
+        apiKey: "AIzaSyCpHYeJmoxC1hdnUOPl1lqI6a3FyUNdDIU",
+        authDomain: "tampbud.firebaseapp.com",
+        databaseURL: "https://tampbud.firebaseio.com",
+        projectId: "tampbud",
+        storageBucket: "tampbud.appspot.com",
+        messagingSenderId: "867512165187"
+    };
+    firebase.initializeApp(config);
+
+  /* 
+    firebase.database().ref('requests').set({
+      createdBy: "Alice Zhu",
+      location: "0,0",
+      timeStamp: Date.now(),
+      completed: false
+    });
+  */
+
+  //AIzaSyDWBOIZ_dwzgmxXJlN6AKpfR1vXd4SBxZU GOogle maps
+  }
+
+
+
   render() {
     return (
         <Router>
