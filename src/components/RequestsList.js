256b02c2bc61cce05c3cee2a4e23ddc5816adee4
Luis Olivar <lao294@nyu.edu>
2018-10-28 03:04:40 -0400

    Google map in requests page

--- src/components/RequestsList.js
+++ src/components/RequestsList.js
@@ -11,6 +11,7 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import { Card } from "rebass";
+import GoogleMapReact from 'google-map-react';
 
 const styles = theme => ({
   root: {
@@ -56,6 +57,13 @@ const styles = theme => ({
 
 function DetailedExpansionPanel(props) {
   const { classes } = props;
+  const defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
   return (
     <div className={classes.root}>
@@ -69,8 +77,14 @@ function DetailedExpansionPanel(props) {
           </div>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className={classes.details}>
-          <div className={classes.column2}>
-            <Card
+          <div style={{ height: '200px', width: '500px' }} className={classes.column2}>
+          <GoogleMapReact
+          bootstrapURLKeys={{ key:"AIzaSyAraoaZOS-5NeVj6waihil6SeGXc9n_N5Q"}}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+          >
+          
+            {/* <Card
               fontSize={6}
               fontWeight="bold"
               p={5}
@@ -78,8 +92,18 @@ function DetailedExpansionPanel(props) {
               bg="#f6f6ff"
               borderRadius={8}
               boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
-            />
-          </div>{" "}
+              lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}>
+              {/* map goes here 
+              </Card>*/}
+              
+              <div lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}></div>
+
+            </GoogleMapReact>
+          </div>
           <div className={classes.column2} />
           <div className={classNames(classes.column2, classes.helper)}>
             <Typography variant="caption">
